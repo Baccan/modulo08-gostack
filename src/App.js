3ba4dca7b0cf0344f8f901da81d05112d94aa60a
Baccan <gustavobaccang@gmail.com>
2019-07-20 23:36:33 +0000

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   // Para cada estado temos um useState() separado e não um objeto único
   // Na primeira posição do array (tech) é o estado própriamente dito, e a segunda posição (setTech) é uma função para atualizar o estado
   const [tech, setTech] = useState([
     // Estado inicial de tech
-    'ReactJS',
-    'React Native',
+    // 'ReactJS',
+    // 'React Native',
   ]);
 
   const [newTech, setNewTech] = useState('');
+
+  // useEffect() sobrepões componentDidMount(), componentDidUpdate(), componentWillUnmount()
+  // O primeiro parametro é a função a ser executada e o segundo é quando será executada
+  // Neste caso, não esta sendo monitorado nenhum estado ou variável, será execudado apenas uma vez
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // componentWillUnmout()
+    // return () => {
+    //   document.removeEventListener()
+    // }
+  }, []);
+
+  // neste caso, todas as vezes que o valor de tech for alterado, a função será executada
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   // this.setState atualizava todos os estados de uma vez. Neste caso utilizamos apenas uma função para um estado específico
   // Por continuar sendo imutável, é precisso copiar todas as informações autuais para atualizar (...tech)
