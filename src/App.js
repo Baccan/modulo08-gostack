e744ed76ad9bce5456e3bda650b0db7d87b75a72
Baccan <gustavobaccang@gmail.com>
2019-07-20 23:23:47 +0000

    Hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  // Para cada estado temos um useState() separado e não um objeto único
+  // Na primeira posição do array (tech) é o estado própriamente dito, e a segunda posição (setTech) é uma função para atualizar o estado
+  const [tech, setTech] = useState([
+    // Estado inicial de tech
+    'ReactJS',
+    'React Native',
+  ]);
+
+  const [newTech, setNewTech] = useState('');
+
+  // this.setState atualizava todos os estados de uma vez. Neste caso utilizamos apenas uma função para um estado específico
+  // Por continuar sendo imutável, é precisso copiar todas as informações autuais para atualizar (...tech)
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+        {/* <li>ReactJS</li>
+      <li>React Native</li>
+      <li>Node.js</li> */}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
